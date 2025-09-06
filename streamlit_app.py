73fdcb16206a976dd36bedb21a0c87276a44814c
blue0jin <blue0jin@gmail.com>
2025-09-06 17:15:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+#import plotly.express as px
 import plotly.graph_objects as go
 import warnings
 from sklearn.model_selection import train_test_split
